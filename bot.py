a434927e0a5cfe6fd0b0fb199b6c770ec0e6afdb
DelusionalOptimist <rudrakshpareek3601@gmail.com>
2020-10-03 04:00:48 +0530

    feat: add xkcd comics

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 import logging
+import random
+import requests
+from bs4 import BeautifulSoup
 from html import escape
 
 from telegram import ParseMode
@@ -37,6 +40,47 @@ def welcome(update, context, new_member):
     context.bot.send_message(chat_id=chat_id, text=text)
 
 
+def xkcd(update, context):
+    message = update.message
+    chat_id = message.chat.id
+    msg_text = message.text
+
+    split_msg = str.split(msg_text)
+    args = split_msg.__len__()
+    if args == 2:
+        logger.info("%s requested an XKCD", escape(message.from_user.username))
+        xkcd_num = split_msg[1]
+        xkcd_url = "https://xkcd.com/" + str(xkcd_num)
+        res = requests.get(xkcd_url)
+        if res.status_code == 404:
+            context.bot.send_message(
+                chat_id=chat_id, text="Sorry, this xkcd comic does not exist yet."
+            )
+            return 1
+
+        xkcd_soup = BeautifulSoup(res.text, "html.parser")
+        comic_elem = xkcd_soup.select("#comic > img:nth-child(1)")
+        comic_url = "https:" + comic_elem[0].get("src")
+
+        context.bot.send_photo(chat_id=chat_id, photo=comic_url)
+
+    else:
+        logger.info("%s requested an XKCD", escape(message.from_user.username))
+
+        random.seed()
+        xkcd_num = random.randint(0, 2300)
+
+        xkcd_url = "https://xkcd.com/" + str(xkcd_num)
+        res = requests.get(xkcd_url)
+        res.raise_for_status()
+
+        xkcd_soup = BeautifulSoup(res.text, "html.parser")
+        comic_elem = xkcd_soup.select("#comic > img:nth-child(1)")
+        comic_url = "https:" + comic_elem[0].get("src")
+
+        context.bot.send_photo(chat_id=chat_id, photo=comic_url)
+
+
 def start(update, context):
 
     chat_id = update.message.chat.id
@@ -52,7 +96,8 @@ def start(update, context):
 def help(update, context):
     help_text = (
         "I understand these commands: \n"
-        "/help - List the commands that I understand \n\n"
+        "/help - List the commands that I understand \n"
+        "/xkcd - Get an xkcd comic. Random if no argument given.\n\n"
         "Contributions from the JODC community helps me in learning more.\n"
         "https://github.com/JIITODC/bot"
     )
@@ -88,6 +133,7 @@ def main():
 
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("xkcd", xkcd))
     dp.add_handler(MessageHandler(Filters.status_update, check))
 
     """updater.start_webhook(listen="0.0.0.0",
