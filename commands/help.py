e6b172dd1841605867684841aa1b7b6156eb5e8d
Yash24 <32770175+Onyx2406@users.noreply.github.com>
2021-08-31 16:20:39 +0530

    updated help.py

--- commands/help.py
+++ commands/help.py
@@ -22,7 +22,7 @@ def help(update, context):
     context.bot.send_message(
         chat_id=chat_id,
         text=help_text,
-        parse_mode=ParseMode.MARKDOWN,
+        parse_mode=ParseMode.HTML,
         disable_web_page_preview=True,
         reply_markup=url_reply_markup
     )
